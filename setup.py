754cc42f838e154c063f593e7ac8be0e7f803c27
Glen Chiacchieri <www@glench.com>
2013-03-12 17:09:17 -0400

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='UsCpi',
-    version='0.2.0',
+    version='0.3.0',
     author='Glen Chiacchieri',
     author_email='www@glench.com',
     packages=['uscpi'],
